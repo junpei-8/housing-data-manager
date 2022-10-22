367b1ccba5636c7728b7ac51fa4e7200c732fb60
junpei-8 <miurajunpeis2@gmail.com>
2022-10-22 23:59:52 +0000

    rename: housing-info-manager => housing data manager

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -5,12 +5,8 @@ import { AppComponent } from './app.component';
 describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [
-        RouterTestingModule
-      ],
-      declarations: [
-        AppComponent
-      ],
+      imports: [RouterTestingModule],
+      declarations: [AppComponent],
     }).compileComponents();
   });
 
@@ -20,16 +16,18 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'housing-info-manager'`, () => {
+  it(`should have as title 'housing-data-manager'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('housing-info-manager');
+    expect(app.title).toEqual('housing-data-manager');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('housing-info-manager app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain(
+      'housing-data-manager app is running!',
+    );
   });
 });
